fb8737d11ed308bca40917df73e0bcaaf92f7ee9
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-11-15 02:03:44 -0500

    Update CartController.java

--- src/Application/CartController.java
+++ src/Application/CartController.java
@@ -1,13 +1,19 @@
 package Application;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.application.Application;
 
-import java.util.Iterator;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class CartController {
 
@@ -19,6 +25,8 @@ public class CartController {
 
     private Order currentOrder;
     private OrderLine currentOrderLine;
+    private OrderController orderController;
+    private Stage secondStage;
 
     void setup(){
         currentOrder = new Order();
@@ -26,27 +34,47 @@ public class CartController {
 
     void add(Sandwich sandwich){
         this.currentOrderLine = new OrderLine(sandwich,sandwich.price());
-        cartOrderView.getItems().add(this.currentOrderLine.toString());
         this.currentOrder.add(currentOrderLine);
+        cartOrderView.getItems().add(this.currentOrder.toString());
+        this.orderTotalCart.setText(String.valueOf(currentOrder.totalPrice()));
     }
 
 
-    public void show(){
-
+    public void show(){//TODO setup interaction
+        //secondStage.show();
+        //firstStage.show();
     }
 
     @FXML
     void addOrderLine(ActionEvent event) {
+        int index;
         MultipleSelectionModel<String> selectionModel = cartOrderView.getSelectionModel();
+        if(selectionModel.isEmpty()){
+            return;
+        }
         ObservableList<String> selectedItems = selectionModel.getSelectedItems();
 
-        currentOrder.add(selectedItems);
-        cartOrderView.getItems().add(this.currentOrderLine.toString());
-        this.orderTotalCart.setText(String.valueOf(currentOrder.totalPrice()));
+        if(selectedItems.get(0).substring(1,2).contains(" ")) {
+            index = Integer.parseInt(selectedItems.get(0).substring(0,1));
+        }
+        else{
+            index = Integer.parseInt(selectedItems.get(0).substring(0,2));
+        }
+
+        //found order line with same serial and put in current order
+        OrderLine lineOrder = currentOrder.find(index);
+
+        //found order line and add identical sandwich to list using add method
+        currentOrder.PrintList();
+        this.add(lineOrder.getSandwich());
     }
 
     @FXML
-    void backToOrder(ActionEvent event) {
+    void backToOrder(ActionEvent event) {//TODO setup interaction
+        //OrderController.show()
+        //firstStage.show();
+        //secondStage.close();
+        return;
 
     }
 
@@ -60,15 +88,65 @@ public class CartController {
     @FXML
     void removeOrderLine(ActionEvent event) {
         MultipleSelectionModel<String> selectionModel = cartOrderView.getSelectionModel();
+        if(selectionModel.isEmpty()){
+            return;
+        }
         ObservableList<String> selectedItems = selectionModel.getSelectedItems();
+        int index;
+        //check for single or double digit serial number
+        if(selectedItems.get(0).substring(1,2).contains(" ")){
+            index = Integer.parseInt(selectedItems.get(0).substring(0,1));
+        }
+        else{
+            index = Integer.parseInt(selectedItems.get(0).substring(0,2));
+        }
 
-        currentOrder.remove(selectedItems);
-        cartOrderView.getItems().remove(selectedItems);
+        OrderLine lineOrder = this.currentOrder.find(index);
+        cartOrderView.getItems().remove(index + " " + lineOrder.toString());
+        this.currentOrder.remove(lineOrder);
+
+
         this.orderTotalCart.setText(String.valueOf(currentOrder.totalPrice()));
     }
 
     @FXML
     void saveOrderToFile(ActionEvent event) {
+        MultipleSelectionModel<String> selectionModel = cartOrderView.getSelectionModel();
+        if(selectionModel.isEmpty()){
+            return;
+        }
 
+        FileChooser fileChooser = new FileChooser();
+        Stage primaryStage = new Stage();
+
+        fileChooser.setTitle("Save File");
+        //adds .txt extension to all text files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        File orderLine = fileChooser.showSaveDialog(primaryStage);
+
+        if(orderLine == null){
+            return;
+        }
+
+        if(selectionModel.isEmpty()){
+            return;
+        }
+        saveTextToFile(this.currentOrder.toString(),orderLine);
+
+    }
+    /**
+     * Saves the text from the line order to a file
+     * @param content,file Takes the strings in the database and file location
+     */
+    private void saveTextToFile(String content, File file){
+        try {
+            PrintWriter writer;
+            writer = new PrintWriter(file);
+            writer.println(content);
+            writer.close();
+        } catch (IOException ex) {
+        }
     }
 }
