6bd7e370776993a4dc4f265fa3ed7e7543c8ada5
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-11-16 18:20:16 -0500

    Update Main.java

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -12,36 +12,37 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			// Load the order scene
-			FXMLLoader orderSceneFXML = new FXMLLoader(getClass().getResource("Order.fxml"));
-			Scene orderScene = (Scene)orderSceneFXML.load();
-			primaryStage.setScene(orderScene);
-			primaryStage.setTitle("Order");
-			
-			// Load the shopping cart scene
-			FXMLLoader cartSceneFXML = new FXMLLoader(getClass().getResource("Cart.fxml"));
-			Scene cartScene = (Scene)cartSceneFXML.load();
-			Stage cartStage = new Stage();
-			cartStage.setScene(cartScene);
-			cartStage.setTitle("Cart");
-			
-			// Pass over the cart controller into the orderController to be used when showing the cart
-			OrderController orderController = orderSceneFXML.<OrderController>getController();
-			CartController cartController = cartSceneFXML.<CartController>getController();
-			
-			orderController.setCartController(cartController);
-			cartController.stagePasser(cartStage);
-			
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the order scene
+            FXMLLoader orderSceneFXML = new FXMLLoader(getClass().getResource("Order.fxml"));
+            Scene orderScene = (Scene)orderSceneFXML.load();
+            primaryStage.setScene(orderScene);
+            primaryStage.setTitle("Order");
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+            // Load the shopping cart scene
+            FXMLLoader cartSceneFXML = new FXMLLoader(getClass().getResource("Cart.fxml"));
+            Scene cartScene = (Scene)cartSceneFXML.load();
+            Stage cartStage = new Stage();
+            cartStage.setScene(cartScene);
+            cartStage.setTitle("Cart");
+
+            // Pass over the cart controller into the orderController to be used when showing the cart
+            OrderController orderController = orderSceneFXML.<OrderController>getController();
+            CartController cartController = cartSceneFXML.<CartController>getController();
+
+
+            orderController.setCartController(cartController);
+            cartController.stagePasser(cartStage);
+            cartController.setup();
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
