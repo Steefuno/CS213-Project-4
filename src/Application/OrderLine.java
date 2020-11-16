dae07f471a6be8fc42646bc9d2d8e14c62ccd057
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-11-15 20:47:34 -0500

    Update OrderLine.java

--- src/Application/OrderLine.java
+++ src/Application/OrderLine.java
@@ -1,7 +1,11 @@
 package Application;
 
-import java.util.Iterator;
+import java.util.StringTokenizer;
 
+/**
+ * This class represents an order line
+ * @author Steven Nguyen, Julian Romero
+ */
 public class OrderLine {
     private int lineNumber;
     private Sandwich sandwich;
@@ -9,11 +13,18 @@ public class OrderLine {
 
     private static int line_number = 1;
 
+    public void setLineNumber(int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
     public OrderLine(Sandwich sandwich, double price) {
         this.lineNumber = line_number;
         line_number++;
         this.sandwich = sandwich;
         this.price = price;
+    }
+    public void decrementSerial(){
+        line_number--;
     }
     public void reset(){
         line_number = 1;
@@ -34,7 +45,8 @@ public class OrderLine {
     // Returns the string form
     @Override
     public String toString() {
-        String result = this.sandwich.toString();
+        String result = this.lineNumber + " ";
+        result += this.sandwich.toString();
         result += " Price $" + this.price;
         return result;
     }
